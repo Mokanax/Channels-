ca9ddac54da910e64a604dfc54e685b9d14c2972
Mokanax <96677491+Mokanax@users.noreply.github.com>
2021-12-28 16:16:43 +1100

    Update and rename Oogg .ts to Popmax.ts

--- Oogg .ts
+++
@@ -1,2 +0,0 @@
-#EXTINF:-1,UK Tiny Pop
-https://tvnow.best/api/stream/ericvanswa65@gmail.com/Shzhzhshzzushzhsi92/livetv.epg/tiny.pop.uk.m3u8
---
+++ Popmax.ts
@@ -0,0 +1,2 @@
+#EXTINF:-1,UK Pop Max
+https://tv4.live/api/stream/ericvanswa65@gmail.com/Shzhzhshzzushzhsi92/livetv.epg/pop.max.uk.m3u8
