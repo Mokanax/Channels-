d058a830527403b79aa79f59ce78d4060a30eab8
Mokanax <96677491+Mokanax@users.noreply.github.com>
2022-01-05 15:13:34 +1100

    Update Cartoon Network (EST).ts

--- Cartoon Network (EST).ts
+++ Cartoon Network (EST).ts
@@ -1,2 +1,2 @@
 #EXTINF:-1, Cartoon Network (EST)
-https://tvnow.best/api/stream/ericvanswa65@gmail.com/Shzhzhshzzushzhsi92/livetv.epg/cartoon.network.usa.eastern.us.m3u8
+http://www.dayiptv.pro:88//04JAN2021A/ZCDQAOZKMJCYGK4/123289
